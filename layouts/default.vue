d7cd8cf7c9ba1fa74df6ed4a439f188608d6bfdc
hlp909 <1015965397@qq.com>
2020-10-05 09:59:24 +0800

    '闲云旅游项目'

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- 头部组件 -->
+    <Header/>
+    <Nuxt />
+    <!-- 底部组件 -->
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// 导入头部组件
+import Header from '@/components/header.vue'
+import Footer from '@/components/footer.vue'
+
+export default {
+  // 注册组件
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+*{
+    margin:0;
+    padding:0;
+}
+    
+a{
+    color: #333;
+    text-decoration: none;
+}
+
+/* a:hover{
+    text-decoration: none;
+} */
+
+ul,li,ol{
+    list-style: none;
+}
+
+i,em{
+   font-style: normal; 
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: '闲云旅游网',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'} // 新增全局字体样式
+
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'assets/main.css' // 新增自定义的页面过渡样式（文件来自3.4.1）
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    transpile: [/^element-ui/],
+  }
+}
