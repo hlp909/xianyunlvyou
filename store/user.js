787641d797d8b7ace60b814d650d1cd2d43de10c
hlp909 <1015965397@qq.com>
2020-10-08 14:50:04 +0800

    '把state替换成函数'

--- store/user.js
+++ store/user.js
@@ -1,13 +1,13 @@
 //store下的每个文件都是一个store的实例，不需要去手创建
 // 只需要保留store的属性，(固定的写法)
 
-export const state={
+export const state=()=>({
     // 采用接口返回的数据结构
     userInfo:{
         token:'',
         user:{}
     }
-}
+})
 
 export const mutations={
      // 保存用户信息到state
