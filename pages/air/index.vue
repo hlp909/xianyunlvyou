7ac8c0fcf457fa72bb8692e76e19d2cd8501712c
hlp909 <1015965397@qq.com>
2020-10-14 13:46:57 +0800

    '推荐机票的列表'

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -36,17 +36,41 @@
         <i>特价机票</i>
       </h2>
       <!-- 特价机票 -->
-      <div class="air-sale">
-
-      </div>
+       <div class="air-sale">
+            <el-row type="flex" class="air-sale-pic" justify="space-between">
+                <el-col :span="6" v-for="(item, index) in sales" :key="index">
+                    <nuxt-link :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`">
+                        <img :src="item.cover"/>
+                        <el-row class="layer-bar" type="flex" justify="space-between">
+                            <span>{{item.departCity}}-{{item.destCity}}</span>
+                            <span>￥{{item.price}}</span>
+                        </el-row>
+                    </nuxt-link>
+                </el-col>
+            </el-row>
+        </div>
   </section>
 </template>
 
 <script>
 import SearchForm from "@/components/air/searchForm";
 export default {
+    data(){
+        return{
+          sales:[]
+        }
+    },
     components:{
       SearchForm
+    },
+    mounted(){
+      // 请求推荐数据的列表
+      this.$axios({
+        url:'/airs/sale'
+      }).then(res=>{
+       
+       this.sales=res.data.data
+      })
     }
 }
 </script>
